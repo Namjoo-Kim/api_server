cef6fc6249240724a16e2b7b66827b2913ced5a5
Namjoo-Kim <skawn8rla@gmail.com>
2022-07-31 01:33:44 +0900

    test

--- main.py
+++ main.py
@@ -25,8 +25,75 @@ class Main:
     def read_item(item_id: str, q: Optional[str] = None):
         """example
         """
-
         return {"item_id": item_id, "q": q}
+    
+    @app.get("/data", summary="예제입니다.")
+    async def data(item_id: str):
+        """data
+        """
+        if item_id == '2' :
+            date = [
+            {
+            "year": '1951 年',
+            "value": 38,
+            },
+            {
+            "year": '1952 年',
+            "value": 52,
+            },
+            {
+            "year": '1956 年',
+            "value": 61,
+            },
+            {
+            "year": '1957 年',
+            "value": 145,
+            },
+            {
+            "year": '1958 年',
+            "value": 48,
+            }
+            ]
+        elif item_id == '3' :
+            date = [
+                {
+                "year": '1Q',
+                "value": 38,
+                },
+                {
+                "year": '2Q',
+                "value": 52,
+                },
+                {
+                "year": '3Q',
+                "value": 61,
+                },
+                {
+                "year": '4Q',
+                "value": 145,
+                }
+            ]
+        elif item_id == '4' :
+            date = [
+                {
+                "year": '25week',
+                "value": 38,
+                },
+                {
+                "year": '26week',
+                "value": 52,
+                },
+                {
+                "year": '27week',
+                "value": 61,
+                },
+                {
+                "year": '28week',
+                "value": 145,
+                }
+            ]
+        return date
+    
 
 #python -m uvicorn main:app --reload
 def main():
