06e4369b475eefbf50f5ef90e07ee39cd7b4bbbf
Namjoo-Kim <skawn8rla@gmail.com>
2022-08-19 01:19:38 +0900

    minor

--- service/sql.py
+++ service/sql.py
@@ -1,39 +1,3 @@
-# from sqlalchemy.orm import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy import create_engine
-# # import pandas as pd
-
-# # Base = declarative_base()
-# # engine = create_engine('mysql+pymysql://root:kim8912!@localhost:8083/knj', echo=True) 
-# # print(engine.execute("select 1").scalar())
-
-# class Sql:
-
-#     def __init__(self):
-#         self.engine = create_engine('mysql+pymysql://root:kim8912!@localhost:8083/knj', echo=True) 
-
-#     def sessionmaker(self):
-#         Session = sessionmaker(bind=self.engine)
-#         session = Session()
-#         return session
-    
-#     def connection(self):
-#         conn = self.engine.connect()
-#         return conn
-    
-#     # def go() :
-#     #     sql = """SELECT `YEAR`, sum(`VALUE`) as `VALUE`
-#     #     FROM (
-#     #         SELECT Concat( WEEK(DATES,1), 'week') AS `YEAR`, CNT as `VALUE`  FROM knjoo_table
-#     #     ) A
-#     #     GROUP BY `YEAR`
-#     #     """ 
-
-#     #     df = pd.read_sql(sql, con=engine)   
-#     #     print(df)
-        
-#     #     return df
-
 import pymysql 
 import os
 from dotenv import load_dotenv
